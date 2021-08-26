be756b5becaca37eab244e58a08af93af8886527
Victoria Serzhanova <62091517+victoriaserzhanova@users.noreply.github.com>
2021-08-26 11:30:13 +0300

    Update choices.js

--- choices.js
+++ choices.js
@@ -1,1 +1,34 @@
 
+        <script src="/choices.min.js"></script>
+  <link rel="stylesheet" href="fonts.css">
+  <link rel="stylesheet" href="style.css">
+
+<select class="form-control"  name="choices-basic" id="choices-basic">
+        <option value="apple">apple</option>
+        <option value="orange">orange</option>
+        <option value="apple">apple</option>
+        <option value="orange">orange</option>
+      </select>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+          const choicesBasic = new Choices('#choices-basic');
+    })
+  </script>
+.choices {
+  width: 176px;
+}
+
+.choices__inner {
+  border-radius: 10px;
+  font-size: 14px;
+  background-color: #ffffff;
+  border: none;
+}
+
+.is-open.is-flipped .choices__inner {
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: 1px solid #CCB26E;
+}
+
+
